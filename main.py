028c7569178e2e8dbd19b81a9aa41208af6db43b
metr-0 <mar2017ar@gmail.com>
2023-06-28 18:51:45 +0300

    Answer from support

--- main.py
+++ main.py
@@ -13,16 +13,28 @@ dp = Dispatcher(bot)
 
 
 @dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+async def welcome(message: types.Message):
     await message.reply("Hello World!")
 
 
 @dp.message_handler(lambda message: message.chat.type == 'private')
-async def echo(message: types.Message):
+async def question(message: types.Message):
     await message.forward(
         chat_id=config['chatId']
     )
     await message.reply('Forwarded!')
+
+
+@dp.message_handler(lambda message: message.chat.id == config['chatId'] and
+                    message.reply_to_message and message.reply_to_message.forward_from)
+async def answer(message: types.Message):
+    user_id = message.reply_to_message.forward_from.id
+    await bot.send_message(
+        chat_id=user_id,
+        text=message.text,
+        reply_to_message_id=message.reply_to_message.forward_from_message_id
+    )
+    await message.reply('Sent!')
 
 
 if __name__ == '__main__':
