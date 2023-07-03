49ce4ae7fec0faff817bd0bc000d7004929d7ffb
metr-0 <mar2017ar@gmail.com>
2023-07-03 08:54:15 +0300

    Getting chat id

--- handlers.py
+++ handlers.py
@@ -10,6 +10,11 @@ async def ping(message: types.Message) -> None:
     await message.reply('pong')
 
 
+@dp.message(Command('id'))
+async def get_id(message: types.Message) -> None:
+    await message.reply(f'Chat ID: {message.chat.id}')
+
+
 @dp.message(F.chat.type == 'private' and Command('start', 'help'))
 async def help_msg(message: types.Message) -> None:
     await message.reply(templates['helpMessage'])
@@ -25,15 +30,11 @@ async def answer(message: types.Message, bot: Bot) -> None:
         chat_id=user_id,
         text=templates['answerReceived']
     )
-    await message.forward(
-        chat_id=user_id
-    )
+    await message.forward(chat_id=user_id)
     await message.reply(templates['answerSent'])
 
 
 @dp.message(F.chat.type == 'private')
 async def question(message: types.Message) -> None:
-    await message.forward(
-        chat_id=config['chatId']
-    )
+    await message.forward(chat_id=config['chatId'])
     await message.reply(templates['questionSent'])
